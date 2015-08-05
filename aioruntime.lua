f1086f6d6260a03e40c63308e039fdf6e3906520
TsT <tst2005@gmail.com>
2015-08-06 00:34:55 +0200

    add the external control module for preload creation

---
+++ aioruntime.lua
@@ -0,0 +1,31 @@
+
+local priorities = {}
+local loadstring=loadstring; local preload = require"package".preload
+
+local _M = {
+	add = function(name, rawcode, pri)
+		print("add", name, #rawcode, pri)
+		local p = priorities[name]
+		if not pri or p and (pri or 0) > p then
+			preload[name] = function(...) return loadstring(rawcode)(...) end
+		else
+			print( ("module %q not overwritten"):format(name), "p", p, "pri", pri )
+		end
+	end,
+}
+return _M
+
+--[[
+local sources, priorities = {}, {}
+]]--
+
+--[[
+local add
+if not pcall(function() add = require"aioruntime".add end) then
+	local loadstring=loadstring; local preload = require"package".preload
+	add = function(name, rawcode)
+		preload[name] = function(...) return loadstring(rawcode)(...) end
+	end
+end
+for name, rawcode in pairs(sources) do add(name, rawcode, priorities[name]) end
+]]--
