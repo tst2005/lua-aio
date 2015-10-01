a1e321afcba44cd73b43d767c6dddb4bfc618670
TsT <tst2005@gmail.com>
2015-10-01 21:46:38 +0200

    move inside src/

--- aio.lua
+++ aio.lua
@@ -11,7 +11,81 @@ _=[[
         exec "$LUA" "$0" "$@"
         exit $?
 ]] and nil
-do local sources, priorities = {}, {};assert(not sources["aio.integrity"])sources["aio.integrity"]=([===[-- <pack aio.integrity> --
+do local sources, priorities = {}, {};assert(not sources["aio"])sources["aio"]=([===[-- <pack aio> --
+_=[[
+        for name in luajit lua5.3 lua-5.3 lua5.2 lua-5.2 lua5.1 lua-5.1 lua; do
+                : ${LUA:="$(command -v "$name")"}
+        done
+        if [ -z "$LUA" ]; then
+                echo >&2 "ERROR: lua interpretor not found"
+                exit 1
+        fi
+        LUA_PATH='./?.lua;./?/init.lua;./lib/?.lua;./lib/?/init.lua;;'
+        exec "$LUA" "$0" "$@"
+        exit $?
+]] and nil
+--[[--------------------------------------------------------------------------
+	-- Dragoon Framework - A Framework for Lua/LOVE --
+	-- Copyright (c) 2014-2015 TsT worldmaster.fr <tst2005@gmail.com> --
+--]]--------------------------------------------------------------------------
+
+local M = {}
+M._NAME = "lua-aio"
+M._VERSION = "lua-aio 0.6"
+M._LICENSE = "MIT"
+
+local core = require("aio.core")
+M.shebang	= assert(core.shebang)
+M.code		= assert(core.code)
+M.codehead	= assert(core.codehead) -- obsolete
+M.shellcode	= assert(core.shellcode)
+
+M.vfile	= assert(core.vfile)
+M.autoaliases	= assert(core.autoaliases)
+M.require	= assert(core.require)
+M.luacode	= assert(core.luacode)
+
+local mods = require "aio.mods"
+M.mode		= assert(mods.mode)
+M.luamod	= assert(mods.luamod)
+M.rawmod	= assert(mods.rawmod)
+M.mod		= assert(mods.mod)
+M.finish	= assert(mods.finish)
+
+local function wrap(f)
+	return function(...)
+		f(...)
+		return M
+	end
+end
+
+for k,v in pairs(M) do
+	if type(v) == "function" then
+		M[k] = wrap(v)
+	end
+end
+
+local integrity = require "aio.integrity"
+if integrity then
+	M.icheck	= assert(integrity.cmd_icheck)
+	M.ichechinit	= assert(integrity.cmd_icheckinit)
+end
+
+local rock = require "aio.rock"
+
+--[[
+for k,v in pairs(rock) do
+	if type(v) == "function" then
+		rock[k] = wrap(v)
+	end
+end
+]]--
+
+M.rock = assert(rock)
+
+return M
+]===]):gsub('\\([%]%[]===)\\([%]%[])','%1%2')
+assert(not sources["aio.integrity"])sources["aio.integrity"]=([===[-- <pack aio.integrity> --
 
 --[[--------------------------------------------------------------------------
 	-- Dragoon Framework - A Framework for Lua/LOVE --
@@ -560,105 +634,6 @@ local rock = {
 
 return rock
 ]===]):gsub('\\([%]%[]===)\\([%]%[])','%1%2')
-assert(not sources["aio.modraw2"])sources["aio.modraw2"]=([===[-- <pack aio.modraw2> --
-
---[[--------------------------------------------------------------------------
-	-- Dragoon Framework - A Framework for Lua/LOVE --
-	-- Copyright (c) 2014-2015 TsT worldmaster.fr <tst2005@gmail.com> --
---]]--------------------------------------------------------------------------
-
-local core = require "aio.core"
-local print_no_nl = assert(core.print_no_nl)
-local autoeol, extractshebang, cat = core.autoeol, core.extractshebang, core.cat
-assert( autoeol and extractshebang and cat )
-
-local integrity = require "aio.integrity"
-local module_with_integrity_check_get = integrity.module_with_integrity_check_get
-local integrity_modcount_incr = integrity.integrity_modcount_incr
-assert( module_with_integrity_check_get and integrity_modcount_incr)
-
-local rawpack2_init_done = false
-local rawpack2_finish_done = false
-
-local function rawpack2_init()
-	print_no_nl([[do local sources, priorities = {}, {};]])
-end
-
-local function rawpack2_module(modname, modpath)
-	assert(modname)
-	assert(modpath)
-
--- quoting solution 1 : prefix all '[', ']' with '\'
---	local quote       = function(s) return s:gsub('([%]%[])','\\%1') end
---	local unquotecode = [[:gsub('\\([%]%[])','%1')]]
-
--- quoting solution 2 : prefix the pattern of '\[===\[', '\]===\]' with '\' ; FIXME: for now it quote \]===\] or \[===\] or \]===\[ or \[===\[
-	local quote       = function(s) return s:gsub('([%]%[]===)([%]%[])','\\%1\\%2') end
-	local unquotecode = [[:gsub('\\([%]%[]===)\\([%]%[])','%1%2')]]
-
-	if not rawpack2_init_done then
-		rawpack2_init_done = not rawpack2_init_done
-		if rawpack2_finish_done then rawpack2_finish_done = false end
-		rawpack2_init()
-	end
-	local b = [[assert(not sources["]] .. modname .. [["])]]..[[sources["]] .. modname .. [["]=(]].."\[===\["
-	local e = "\]===\])".. unquotecode
-
-	local d = "-- <pack "..modname.."> --" -- error message keep the first 45 chars max
-	print_no_nl(
-		b .. d .."\n"
-		.. quote(autoeol(extractshebang(cat(modpath))))
-		.. e .."\n"
-	)
-	--integrity_modcount_incr() -- for integrity check
-end
-
---local function rawpack2_finish()
---	print_no_nl(
---[[
---local loadstring=_G.loadstring or _G.load; local preload = require"package".preload
---for name, rawcode in pairs(sources) do preload[name]=function(...)return loadstring(rawcode)(...)end end
---end;
---]]
---)
---end
-
-local function rawpack2_finish()
-	print_no_nl(
-[[
-local add
-if not pcall(function() add = require"aioruntime".add end) then
-        local loadstring=_G.loadstring or _G.load; local preload = require"package".preload
-        add = function(name, rawcode)
-		if not preload[name] then
-		        preload[name] = function(...) return assert(loadstring(rawcode), "loadstring: "..name.." failed")(...) end
-		else
-			print("WARNING: overwrite "..name)
-		end
-        end
-end
-for name, rawcode in pairs(sources) do add(name, rawcode, priorities[name]) end
-end;
-]]
-)
-end
-
-local function finish()
-	if rawpack2_init_done and not rawpack2_finish_done then
-		rawpack2_finish_done = not rawpack2_finish_done
-		rawpack2_finish()
-	end
-end
-
-------------------------------------------------------------------------------
-
-local M = {}
-
-M.pack_mod	= assert(rawpack2_module)
-M.pack_finish	= assert(finish)
-
-return M
-]===]):gsub('\\([%]%[]===)\\([%]%[])','%1%2')
 assert(not sources["aio.modraw"])sources["aio.modraw"]=([===[-- <pack aio.modraw> --
 
 --[[--------------------------------------------------------------------------
@@ -776,77 +751,102 @@ local _M = {}
 _M.pack_mod = cmd_luamod
 return _M
 ]===]):gsub('\\([%]%[]===)\\([%]%[])','%1%2')
-assert(not sources["aio"])sources["aio"]=([===[-- <pack aio> --
-_=[[
-        for name in luajit lua5.3 lua-5.3 lua5.2 lua-5.2 lua5.1 lua-5.1 lua; do
-                : ${LUA:="$(command -v "$name")"}
-        done
-        if [ -z "$LUA" ]; then
-                echo >&2 "ERROR: lua interpretor not found"
-                exit 1
-        fi
-        LUA_PATH='./?.lua;./?/init.lua;./lib/?.lua;./lib/?/init.lua;;'
-        exec "$LUA" "$0" "$@"
-        exit $?
-]] and nil
+assert(not sources["aio.modraw2"])sources["aio.modraw2"]=([===[-- <pack aio.modraw2> --
+
 --[[--------------------------------------------------------------------------
 	-- Dragoon Framework - A Framework for Lua/LOVE --
 	-- Copyright (c) 2014-2015 TsT worldmaster.fr <tst2005@gmail.com> --
 --]]--------------------------------------------------------------------------
 
-local M = {}
-M._NAME = "lua-aio"
-M._VERSION = "lua-aio 0.6"
-M._LICENSE = "MIT"
-
-local core = require("aio.core")
-M.shebang	= assert(core.shebang)
-M.code		= assert(core.code)
-M.codehead	= assert(core.codehead) -- obsolete
-M.shellcode	= assert(core.shellcode)
-
-M.vfile	= assert(core.vfile)
-M.autoaliases	= assert(core.autoaliases)
-M.require	= assert(core.require)
-M.luacode	= assert(core.luacode)
-
-local mods = require "aio.mods"
-M.mode		= assert(mods.mode)
-M.luamod	= assert(mods.luamod)
-M.rawmod	= assert(mods.rawmod)
-M.mod		= assert(mods.mod)
-M.finish	= assert(mods.finish)
-
-local function wrap(f)
-	return function(...)
-		f(...)
-		return M
-	end
-end
-
-for k,v in pairs(M) do
-	if type(v) == "function" then
-		M[k] = wrap(v)
-	end
-end
+local core = require "aio.core"
+local print_no_nl = assert(core.print_no_nl)
+local autoeol, extractshebang, cat = core.autoeol, core.extractshebang, core.cat
+assert( autoeol and extractshebang and cat )
 
 local integrity = require "aio.integrity"
-if integrity then
-	M.icheck	= assert(integrity.cmd_icheck)
-	M.ichechinit	= assert(integrity.cmd_icheckinit)
+local module_with_integrity_check_get = integrity.module_with_integrity_check_get
+local integrity_modcount_incr = integrity.integrity_modcount_incr
+assert( module_with_integrity_check_get and integrity_modcount_incr)
+
+local rawpack2_init_done = false
+local rawpack2_finish_done = false
+
+local function rawpack2_init()
+	print_no_nl([[do local sources, priorities = {}, {};]])
 end
 
-local rock = require "aio.rock"
+local function rawpack2_module(modname, modpath)
+	assert(modname)
+	assert(modpath)
 
+-- quoting solution 1 : prefix all '[', ']' with '\'
+--	local quote       = function(s) return s:gsub('([%]%[])','\\%1') end
+--	local unquotecode = [[:gsub('\\([%]%[])','%1')]]
+
+-- quoting solution 2 : prefix the pattern of '\[===\[', '\]===\]' with '\' ; FIXME: for now it quote \]===\] or \[===\] or \]===\[ or \[===\[
+	local quote       = function(s) return s:gsub('([%]%[]===)([%]%[])','\\%1\\%2') end
+	local unquotecode = [[:gsub('\\([%]%[]===)\\([%]%[])','%1%2')]]
+
+	if not rawpack2_init_done then
+		rawpack2_init_done = not rawpack2_init_done
+		if rawpack2_finish_done then rawpack2_finish_done = false end
+		rawpack2_init()
+	end
+	local b = [[assert(not sources["]] .. modname .. [["])]]..[[sources["]] .. modname .. [["]=(]].."\[===\["
+	local e = "\]===\])".. unquotecode
+
+	local d = "-- <pack "..modname.."> --" -- error message keep the first 45 chars max
+	print_no_nl(
+		b .. d .."\n"
+		.. quote(autoeol(extractshebang(cat(modpath))))
+		.. e .."\n"
+	)
+	--integrity_modcount_incr() -- for integrity check
+end
+
+--local function rawpack2_finish()
+--	print_no_nl(
 --[[
-for k,v in pairs(rock) do
-	if type(v) == "function" then
-		rock[k] = wrap(v)
+--local loadstring=_G.loadstring or _G.load; local preload = require"package".preload
+--for name, rawcode in pairs(sources) do preload[name]=function(...)return loadstring(rawcode)(...)end end
+--end;
+--]]
+--)
+--end
+
+local function rawpack2_finish()
+	print_no_nl(
+[[
+local add
+if not pcall(function() add = require"aioruntime".add end) then
+        local loadstring=_G.loadstring or _G.load; local preload = require"package".preload
+        add = function(name, rawcode)
+		if not preload[name] then
+		        preload[name] = function(...) return assert(loadstring(rawcode), "loadstring: "..name.." failed")(...) end
+		else
+			print("WARNING: overwrite "..name)
+		end
+        end
+end
+for name, rawcode in pairs(sources) do add(name, rawcode, priorities[name]) end
+end;
+]]
+)
+end
+
+local function finish()
+	if rawpack2_init_done and not rawpack2_finish_done then
+		rawpack2_finish_done = not rawpack2_finish_done
+		rawpack2_finish()
 	end
 end
-]]--
 
-M.rock = assert(rock)
+------------------------------------------------------------------------------
+
+local M = {}
+
+M.pack_mod	= assert(rawpack2_module)
+M.pack_finish	= assert(finish)
 
 return M
 ]===]):gsub('\\([%]%[]===)\\([%]%[])','%1%2')
--- make-all-in-one.sh
+++ make-all-in-one.sh
@@ -6,6 +6,7 @@ cd -- "$(dirname "$0")" || exit 1
 # wget https://raw.githubusercontent.com/tst2005/lua-aio/aio.lua
 
 export LUA_PATH="./?.lua;./?/init.lua;"\
+"./src/?.lua;./src/?/init.lua;"\
 "../lua-?/?.lua;../lua-?/?/init.lua;"\
 "../lua-?/lua/?.lua;../lua-?/lua/?/init.lua;"\
 "../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;"
@@ -16,24 +17,25 @@ local aio = require "aio.init"
 aio.mode("raw2")
 
 --aio.rock.auto("rockspecs/aio-0.6.0-0.rockspec.draft", "aio")
-aio.mod("aio.core",             "aio/core.lua")
-aio.mod("aio.mods",		"aio/modlua.lua")
-aio.mod("aio.modlua", 		"aio/modlua.lua")
-aio.mod("aio.modraw", 		"aio/modraw.lua")
-aio.mod("aio.modraw2", 		"aio/modraw2.lua")
-aio.mod("aio.rock",		"aio/rock.lua")
-aio.mod("aio.integrity",	"aio/integrity.lua")
+aio.mod("aio.core",             "lib/aio/core.lua")
+aio.mod("aio.mods",		"lib/aio/modlua.lua")
+aio.mod("aio.modlua", 		"lib/aio/modlua.lua")
+aio.mod("aio.modraw", 		"lib/aio/modraw.lua")
+aio.mod("aio.modraw2", 		"lib/aio/modraw2.lua")
+aio.mod("aio.rock",		"lib/aio/rock.lua")
+aio.mod("aio.integrity",	"lib/aio/integrity.lua")
 aio.finish()
 
 aio.mod("compat_env",		"compat_env.lua")
 aio.finish()
 
-aio.code(			"aio/init.lua")
+aio.code(			"lib/aio/init.lua")
 aio.finish()
 ' > aio-wdeps/aio.lua
 fi
 
-lua -e '
+#LUA_PATH="$LUA_PATH"
+luajit -e '
 local aio = require "aio.init"
 aio.mode("raw2")
 aio.rock.auto("rockspecs/aio-0.6.0-0.rockspec.draft", "aio")
