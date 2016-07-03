0fb7d276a7623e66ea17be27e31516ef3257c479
TsT <tst2005@gmail.com>
2016-07-04 00:38:46 +0200

    new aio !

--- NG/make-all-in-one.sh
+++ NG/make-all-in-one.sh
@@ -3,16 +3,16 @@
 cd -- "$(dirname "$0")" || exit 1
 
 # see https://github.com/tst2005/lua-aio
-# wget https://raw.githubusercontent.com/tst2005/lua-aio/aio.lua
+# wget https://raw.githubusercontent.com/tst2005/lua-aio/aio.lua #FIXME
 
 luajit -e '
-local aio = require "aio-only"
+local aio = require "aio-bootstrap"
 --aio.mode("raw2")
 aio.mod("mini.compat-env",	"mini/compat-env.lua")
 aio.mod("mini.class", 		"mini/class.lua")
 aio.finish()
 
-aio.code(			"aio-only.lua")
+aio.code(			"aio-bootstrap.lua")
 aio.finish()
 '  > aio-full.lua
 
