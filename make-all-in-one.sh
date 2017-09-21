91a39500083716be0c74b10e968ecb2ad1566b94
TsT <tst2005@gmail.com>
2017-09-21 10:57:28 +0200

    rename result directory

--- make-all-in-one.sh
+++ make-all-in-one.sh
@@ -11,10 +11,10 @@ export LUA_PATH="./?.lua;./?/init.lua;"\
 "../lua-?/lua/?.lua;../lua-?/lua/?/init.lua;"\
 "../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;"
 
-[ -d allinone ] || mkdir allinone
+[ -d generated-bundle ] || mkdir generated-bundle
 
 luajit -e '
 local aio = require "aio.init"
 aio.mode("raw2")
 aio.rock.auto("rockspecs/aio-0.6.3-0.rockspec.draft", "aio")
-' > allinone/aio.lua
+' > generated-bundle/aio.lua
